14a8cbaf71f5df14961fb3a0653b5158513c7dcd
hookookoo-fin <hookookoo@gmail.com>
2026-03-26 12:33:55 +0200

    Files uploaded
    1st version

---
+++ sw.js
@@ -0,0 +1,37 @@
+const cacheName = 'haukku-v16'; // MUISTA KASVATTAA TÄTÄ NUMEROA AINA KUN PÄIVITÄT!
+const assets = [
+  './', 
+  './index.html', 
+  './manifest.json', 
+  './icon.png', 
+  './ohjeet.html', // Lisätty uusi tiedosto listaan
+  './koira.jpg'   //Lisätty uusi tiedosto listaan
+]; // sw.js poistettu tästä listasta
+
+// ... loppuosa sw.js-tiedostosta säilyy samana ...
+
+self.addEventListener('install', e => {
+  // Pakottaa uuden Service Workerin aktivoitumaan heti
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('activate', e => {
+  // Poistaa vanhat välimuistit automaattisesti
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
