34f78c71774732f092445e2bbcc854efe4443769
hookookoo-fin <hookookoo@gmail.com>
2026-03-28 17:13:38 +0200

    v25. Versio näkyviin. Värisäätöä.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = 'haukku-v21'; // MUISTA KASVATTAA TÄTÄ NUMEROA AINA KUN PÄIVITÄT!
+const cacheName = 'haukku-v25'; // MUISTA KASVATTAA TÄTÄ NUMEROA AINA KUN PÄIVITÄT! PÅIVITÄ MYÖS INDEX.HTML VERSIO
 const assets = [
   'index.html', 
   'manifest.json', 
